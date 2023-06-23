f80af3189330480ffc116de0c3f0fb74ef250c52
Ömer Halit Çinar <omerhalitcinar@edu.bme.hu>
2023-06-23 20:13:25 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def bineary():
+    print("is it bigger than 10?")
+    answer = input()
+    print(answer)
+
+    if answer == 'yes':
+        print("Is it bigger than 50?")
+        answer = input()
+        print(answer)
+
+
+bineary()

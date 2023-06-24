5cbe3829e29dbe3253a491d32b6862b82499d45b
Ömer Halit Çinar <omerhalitcinar@edu.bme.hu>
2023-06-24 20:10:38 +0200

    added binary search

---
+++ binarySearch.py
@@ -0,0 +1,16 @@
+def binary_search(list, target):
+    first = 0
+    last = len(list)-1
+
+    while (first <= last):
+        middle = (last + first) // 2
+        if target == list[middle]:
+            return middle
+        elif target > list[middle]:
+            first = middle + 1
+        else:
+            last = middle - 1
+
+list = [1,2,3,4,5,6,7,8,9]
+target = 6
+print(binary_search(list, target))

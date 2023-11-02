d0a7b21540d79719108c8dec551b3987aab4a541
Ömer Halit Cinar <102431713+omerhalid@users.noreply.github.com>
2023-11-03 00:06:40 +0100

    Add files via upload

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -2,6 +2,14 @@
 #Link: https://leetcode.com/problems/product-of-array-except-self/
 
 #High level solution for the below code using postfix and prefix products:
+# 1. Create an array of 1s with the same length as the input array
+# 2. Set a prefix variable to 1
+# 3. Iterate through the input array and set the current index of the answers array to the prefix variable
+# 4. Multiply the prefix variable by the current element in the input array
+# 5. Set a postfix variable to 1
+# 6. Iterate through the input array backwards and multiply the current index of the answers array by the postfix variable
+# 7. Multiply the postfix variable by the current element in the input array
+# 8. Return the answers array
 
 
 def productExceptSelf(nums):

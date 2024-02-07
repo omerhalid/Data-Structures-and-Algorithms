d6f2fdf0b652f8faf36ca34d6ad6f7f2b95ee2fe
omerhalid <omerhalidcinar@gmail.com>
2024-02-07 02:34:04 +0100

    Implement quicksort algorithm in Python

--- sorting_algo/quick_sort.py
+++ sorting_algo/quick_sort.py
@@ -1,1 +1,31 @@
 # Quick sort algorithm
+# def quicksort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[len(arr) // 2]
+#     left = [x for x in arr if x < pivot]
+#     middle = [x for x in arr if x == pivot]
+#     right = [x for x in arr if x > pivot]
+#     return quicksort(left) + middle + quicksort(right)
+
+# print(quicksort([3,6,8,10,1,2,1]))
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quicksort(arr, low, pi-1)
+        quicksort(arr, pi+1, high)
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i + 1
+
+data = [3,6,8,10,1,2,1]
+quicksort(data, 0, len(data)-1)
+print(data)
